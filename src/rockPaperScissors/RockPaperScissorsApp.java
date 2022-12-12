6a63d5c593271054ca2db25ffef2d1b8fc81452a
Randamus <52765966+rcviets@users.noreply.github.com>
2022-12-11 17:51:00 -0700

    enum complete

--- src/rockPaperScissors/RockPaperScissorsApp.java
+++ src/rockPaperScissors/RockPaperScissorsApp.java
@@ -1,25 +1,15 @@
-/**
- * 
- */
 package rockPaperScissors;
 
-/**
- * @author Randa Viets & Samuel Franks
- *
- */
 public class RockPaperScissorsApp {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		RockPaperScissors throw1 = new RockPaperScissors(ThrowType.ROCK);
-		RockPaperScissors throw2 = new RockPaperScissors(ThrowType.PAPER);
+		RockPaperScissors t1 = new RockPaperScissors(ThrowType.ROCK);
+		RockPaperScissors t2 = new RockPaperScissors(ThrowType.PAPER);
 		
-		System.out.println("Throw 1: " + throw1);
-		System.out.println("Throw 2: " + throw2);
+		System.out.println("t1: " + t1);
+		System.out.println("t2: " + t2);
 		
-		System.out.println("Throw 1 Equals Throw 2 ? : " + throw1.equals(throw2));
-	}
+		System.out.println("t1.equals(t2) = " + t1.equals(t2));
+	} 
 
 }
